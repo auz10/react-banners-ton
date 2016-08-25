ff83ee5aeacee9b6c2b31918254a8ea6c878ae97
Austin Limmer <austin.limmer@net-a-porter.com>
2016-08-25 13:01:30 +0100

    add onclick for more item info

--- src/components/PromoBlock.js
+++ src/components/PromoBlock.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
 
 class PromoBlock extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            promoClicked: false
+        };
+    }
+    handleClick () {
+        console.log(this.props.promoKey)
+        this.setState({ promoClicked: !this.state.promoClicked })
+    }
+    renderBody() {  
+        if (this.state.promoClicked) {
+            return (
+                <div className="panel-body">
+                    <img src={this.props.imagePath} className="promo-image lrg" alt={this.props.name} />
+                    <p>{this.props.head}</p>
+                    <p>{this.props.sell}</p>
+                    <p>{this.props.url}</p>
+                    <p>{this.props.cta}</p>
+                </div>
+             )
+        } else {
+            return (
+                <div className="panel-body">
+                    <img src={this.props.imagePath} className="promo-image" alt={this.props.name} />
+                    <p>{this.props.head}</p>
+                    <p>{this.props.sell}</p>
+                </div>
+             )
+        }      
+    }
     render () {
         return (
             <div className="result-panel">
-                <div className="panel panel-default">
-                    <div className="panel-heading">{this.props.promoKey}</div>
-                        <div className="panel-body">
-                            <img src={this.props.imagePath} className="promo-image" alt={this.props.name} />
-                            <p>{this.props.head}</p>
-                            <p>{this.props.sell}</p>
-                        </div>
+                <div className="panel panel-default" onClick={this.handleClick.bind(this)}>
+                    <div className="panel-heading">{this.props.promoKey}<span>{this.props.channel.join('/')}</span></div>
+                        {this.renderBody()}
                 </div>
             </div>
         )
