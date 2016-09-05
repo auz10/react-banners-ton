33f29d5be6961ef2576b49c923aa23ae1185657f
Austin Limmer <austin.limmer@net-a-porter.com>
2016-09-05 17:06:44 +0100

    change promo layout

--- src/components/PromoBlock.js
+++ src/components/PromoBlock.js
@@ -3,19 +3,27 @@ import React, { Component } from 'react';
 class PromoBlock extends Component {
     constructor(){
         super()
-        this.state = { active: true }
+        this.state = { active: false }
     }
     switchActive () {
+        console.log(1)
         this.setState({ active: !this.state.active });
+    }
+    saveInfo (infoItem) {
+        console.log('promoBLock'+infoItem)
+    }
+    updatePromoField(value) {
+        console.log('upv: '+value)
     }
     renderPromoInfo () {
         if(this.state.active) {
             return <PromoInfo>
-                <InfoItem typeText="Head" defaultValue={this.props.head}/>
-                <InfoItem typeText="Sell" defaultValue={this.props.sell}/>
-                <InfoItem typeText="CTA" defaultValue={this.props.cta}/>
-                <InfoItem typeText="URL" defaultValue={this.props.url}/>
-                <InfoItem typeText="Image" defaultValue={this.props.imagePath}/>
+                <InfoItem typeText="Head" defaultValue={this.props.head} updatePromoField={this.updatePromoField}/>
+                <InfoItem typeText="Sell" defaultValue={this.props.sell} updatePromoField={this.updatePromoField}/>
+                <InfoItem typeText="CTA" defaultValue={this.props.cta} updatePromoField={this.updatePromoField}/>
+                <InfoItem typeText="URL" defaultValue={this.props.url} updatePromoField={this.updatePromoField}/>
+                <InfoItem typeText="Image" defaultValue={this.props.imagePath} updatePromoField={this.updatePromoField}/>
+                <SaveButton saveInfo={this.saveInfo} />
             </PromoInfo>
         }
     }
@@ -23,10 +31,10 @@ class PromoBlock extends Component {
         return (
             <div className="result-panel">
                 <div className="panel panel-default">
-                    <PromoHeading onClick={console.log(1)}>
+                    <div className="panel-heading" onClick={this.switchActive.bind(this)}>
                         {this.props.promoKey}
                         <span>{this.props.channel.join('/')}</span>
-                    </PromoHeading>
+                    </div>
                     <PromoBody active={this.state.active}>
                         <PromoImage image={this.props.imagePath} alt={this.props.name}/>
                         {this.renderPromoInfo()}
@@ -49,11 +57,49 @@ class PromoBody extends Component {
     }
 }
 
-const PromoHeading = (props) => (
-    <div className="panel-heading">
-        {props.children}
-    </div>
-);
+class InfoItem extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            inputValue: this.props.defaultValue
+        }
+    }
+    updateField (e) {
+        this.setState({inputValue: e.target.value})
+        this.props.updatePromoField(this.state.inputValue)
+    }
+    render () {
+        return (
+            <div className="form-group">
+                <input type="text" 
+                    className="form-control form-control-sm"
+                    value={this.state.inputValue} 
+                    placeholder={this.props.typeText}
+                    onChange={this.updateField.bind(this)}
+                />
+            </div>
+        )
+    }
+}
+
+class SaveButton extends Component {
+    constructor(props) {
+        super(props)
+        console.log(props)
+    }
+    handleSubmit() {
+        this.props.saveInfo("yo")
+    }
+    render () {
+        return (
+            <button type="button" 
+                    className="btn btn-primary save-btn"
+                    onClick={this.handleSubmit.bind(this)}>
+                        Save
+            </button>
+        )
+    }
+}
 
 const PromoImage = (props) => (
     <div className="left-col">
@@ -63,17 +109,10 @@ const PromoImage = (props) => (
 
 const PromoInfo = (props) => (
     <div className="right-col">
-        <ul className="list-group">
-            {props.children}
-        </ul>
+        <form>
+        {props.children}
+        </form>
     </div>
-);
-
-const InfoItem = (props) => (
-    <li className="list-group-item">
-        <div className="input-key">{props.typeText}</div>
-        {props.defaultValue}
-    </li>
 );
 
 export default PromoBlock;
